77cd5ee3a6dd2dd02d51ca4368aabfaad98da048
Jeremy Moyers <jmoyers14@gmail.com>
2016-12-15 13:13:58 -0800

    parse device list from hockey

--- lib/zamboni_ci.rb
+++ lib/zamboni_ci.rb
@@ -1,38 +1,24 @@
 require 'optparse'
 require 'curb'
 require "./zamboni_ci/command_parser"
+require "./zamboni_ci/device_parser"
 
+#parse command line arguments
 arg_array = ARGV
-
 options = CommandParser.parse arg_array
 
-puts options.hockey_app_id
-puts options.hockey_auth_token
-puts options.hockey_session_token
-
-#hockey_url = "https://rink.hockeyapp.net/manage/apps/#{options[:hockey_app_id]}/app_users/export"
+hockey_url = "https://rink.hockeyapp.net/manage/apps/#{options.hockey_app_id}/app_users/export"
 
 # request UDID list for rocketing app
-http = Curl.post("https://rink.hockeyapp.net/manage/apps/444228/app_users/export", {:_method => "post", :authenticity_token => "yxwlg70jsOugsiWyBOGNZTy8e16q2kylCSc7lgRJeig="}) do |http|
-   http.headers['Cookie'] = "_hockey_app_session=8b03db099b5495760d2afd24994548d8;"
+http = Curl.post(hockey_url, {:_method => "post", :authenticity_token => options.hockey_auth_token}) do |http|
+   http.headers['Cookie'] = "_hockey_app_session=#{options.hockey_session_token};"
    http.headers['Content_Type'] = "application/x-www-form-urlencoded"
    http.headers['Accept'] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
 end
 
+hockey_devices = DeviceParser.parse(http.body_str)
+
 # Parse hockey UDIDs into an array
-existing_hockey_udids = Array.new
-
-index = 0
-http.body_str.each_line do |line|
-   if index > 0
-      parsed = line.split
-      existing_hockey_udids.push parsed.first
-   end
-   index = index + 1      
-end
-
-puts existing_hockey_udids
-
 =begin
 puts ENV["ZAMBONI_APPLE_ID_EMAIL"]
 puts ENV["ZAMBONI_APPLE_ID_PASSWORD"]
