69735eff31197a9c2208139e78e17dc46bab0ac6
Jeremy Moyers <jmoyers14@gmail.com>
2016-12-15 15:01:35 -0800

    update device parser tests

--- test/tc_device_parser.rb
+++ test/tc_device_parser.rb
@@ -3,7 +3,7 @@ require '../lib/zamboni_ci/device_parser'
 
 class TC_device_parser < Test::Unit::TestCase
    def test_empty_string
-      devices = DeviceParser.parse("")
+      devices = DeviceParser.parse_hockey("")
       assert(devices.count == 0, "device array should be empty")
    end
 
@@ -12,7 +12,7 @@ class TC_device_parser < Test::Unit::TestCase
       d19e21f58096d3f81124e7200a74dbdb276c3cdc  Jeremy Moyers iPhone 5s
       ea9b0232a72f31efc28724580dff4c8beec98361  Some unknown
       c1f43d905c1e1c05badda45745d8f3438a892b93  Some unknown"
-      devices = DeviceParser.parse(input)
+      devices = DeviceParser.parse_hockey(input)
       
       assert(devices.count == 3, "devices should have three elements")
       assert(devices[0].udid == "d19e21f58096d3f81124e7200a74dbdb276c3cdc", "device one")
@@ -27,7 +27,7 @@ class TC_device_parser < Test::Unit::TestCase
       input = "d19e21f58096d3f81124e7200a74dbdb276c3cdc  Jeremy Moyers iPhone 5s
       ea9b0232a72f31efc28724580dff4c8beec98361  Some unknown
       c1f43d905c1e1c05badda45745d8f3438a892b93  Some unknown"
-      devices = DeviceParser.parse(input)
+      devices = DeviceParser.parse_hockey(input)
 
       assert(devices.count == 3, "devices should have three elements")
       assert(devices[0].udid == "d19e21f58096d3f81124e7200a74dbdb276c3cdc", "device one")
@@ -40,14 +40,14 @@ class TC_device_parser < Test::Unit::TestCase
 
    def test_only_header
       input = "Device ID   Device Name"
-      devices = DeviceParser.parse(input)
+      devices = DeviceParser.parse_hockey(input)
       assert(devices.count == 0, "device array should be empty")
    end
 
    def test_device_no_name
       input = "Device ID   Device Name
       d19e21f58096d3f81124e7200a74dbdb276c3cdc"
-      devices = DeviceParser.parse(input)
+      devices = DeviceParser.parse_hockey(input)
       assert(devices.count == 1, "devices should have three elements")
       assert(devices[0].udid == "d19e21f58096d3f81124e7200a74dbdb276c3cdc", "")
    end
