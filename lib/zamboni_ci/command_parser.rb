22c2a370654d95c358478ea0b8d52a8ee5c643a3
Jeremy Moyers <jmoyers14@gmail.com>
2016-12-16 12:08:07 -0800

    remove session_token and auth_token from command_parser

--- lib/zamboni_ci/command_parser.rb
+++ lib/zamboni_ci/command_parser.rb
@@ -1,6 +1,6 @@
 require 'optparse'
 
-Options = Struct.new(:hockey_app_id, :hockey_auth_token, :hockey_session_token, :bundle_id)
+Options = Struct.new(:hockey_app_id, :bundle_id)
 
 class CommandParser
    def self.parse(options)
@@ -9,16 +9,8 @@ class CommandParser
       opt_parser = OptionParser.new do |parser|
          parser.banner = "Usage: example"
 
-         parser.on("--h_app_id HOCKEYAPPID", "apple id with itunes connect access.") do |v|
+         parser.on("--h_app_id HOCKEYAPPID", "Hockey app id") do |v|
             args.hockey_app_id = v
-         end
-
-         parser.on("--h_auth_token HOCKEYAUTHTOKEN", "hockey authentication token") do |v|
-            args.hockey_auth_token = v
-         end
-
-         parser.on("--h_session_token HOCKEYSESSIONTOKEN", "hockey app session token") do |v|
-            args.hockey_session_token = v
          end
 
          parser.on("--bundle_id BUNDLEID", "provisioning profile's bundle identifier") do |v|
--- test/tc_command_parser.rb
+++ test/tc_command_parser.rb
@@ -6,48 +6,30 @@ class TC_command_parser < Test::Unit::TestCase
    def test_no_args
       options = CommandParser.parse []
       assert(options.hockey_app_id == nil, 'hockey_app_id is not nil')
-      assert(options.hockey_auth_token == nil, 'hockey_auth_token is not nil')
-      assert(options.hockey_session_token == nil, 'hockey_auth_token is not nil')
       assert(options.bundle_id == nil, 'bundle is not nil')
    end
 
    def test_all_args
-      options = CommandParser.parse %w[--h_app_id 123456 --h_auth_token abcdefg --h_session_token lmnop --bundle_id com.bundle.id]
+      options = CommandParser.parse %w[--h_app_id 123456 --bundle_id com.bundle.id]
       assert(options.hockey_app_id == "123456", 'hockey_app_id not parsed')
-      assert(options.hockey_auth_token == "abcdefg", 'hockey_auth_token not parsed')
-      assert(options.hockey_session_token == "lmnop", 'hockey_auth_token not parsed')
       assert(options.bundle_id == "com.bundle.id", 'bundle_id not parsed')
    end
 
    def test_only_app_id
       options = CommandParser.parse %w[--h_app_id 123456]
       assert(options.hockey_app_id == "123456", 'hockey_app_id not parsed')
-      assert(options.hockey_auth_token == nil, 'hockey_auth_token not nil')
-      assert(options.hockey_session_token == nil, 'hockey_auth_token not nil')
       assert(options.bundle_id == nil, 'bundle_id not nil')
    end
 
-   def test_only_auth_token
-      options = CommandParser.parse %w[--h_auth_token abcdefg]
+   def test_only_bundle_id
+      options = CommandParser.parse %w[--bundle_id com.bundle.id]
       assert(options.hockey_app_id == nil, 'hockey_app_id not nil')
-      assert(options.hockey_auth_token == "abcdefg", 'hockey_auth_token not parsed')
-      assert(options.hockey_session_token == nil, 'hockey_auth_token not nil')
-      assert(options.bundle_id == nil, 'bundle_id not nil')
-   end
-
-   def test_only_session_token
-      options = CommandParser.parse %w[--h_session_token lmnop]
-      assert(options.hockey_app_id == nil, 'hockey_app_id not nil')
-      assert(options.hockey_auth_token == nil, 'hockey_auth_token not nil')
-      assert(options.hockey_session_token == "lmnop", 'hockey_auth_token not parsed')
-      assert(options.bundle_id == nil, 'bundle_id not nil')
+      assert(options.bundle_id == "com.bundle.id", 'bundle_id id not parsed')
    end
 
    def test_all_args_re_order
-      options = CommandParser.parse %w[--bundle_id com.bundle.id --h_auth_token abcdefg --h_app_id 123456 --h_session_token lmnop]
+      options = CommandParser.parse %w[--bundle_id com.bundle.id --h_app_id 123456]
       assert(options.hockey_app_id == "123456", 'hockey_app_id not parsed')
-      assert(options.hockey_auth_token == "abcdefg", 'hockey_auth_token not parsed')
-      assert(options.hockey_session_token == "lmnop", 'hockey_auth_token not parsed')
       assert(options.bundle_id == "com.bundle.id", 'bundle_id not parsed')
    end
 
